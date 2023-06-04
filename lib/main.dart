29e904add8d46690e695355d7f44fcc1ba856f49
slimpotatoboy <edwinmhzn@gmail.com>
2023-06-04 10:24:57 +0545

    Text and buttons added

--- lib/main.dart
+++ lib/main.dart
@@ -38,14 +38,98 @@ class MyApp extends StatelessWidget {
         ),
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.network(
-                "https://dipenmaharjan.com.np/thumbnail.png",
+                "https://dipenmaharjan.com.np/shreeamba.jpg",
                 width: MediaQuery.of(context).size.width,
                 height: 100,
                 // compares to object-fit in css
                 fit: BoxFit.cover,
               ),
+              Text(
+                "Kapla Valor",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "5.0 Stars",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "\$5000",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const Text(
+                "Description",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              const Text(
+                "Lorem Ipsu Lorem Ipsu Lorem Ipsu Lorem Ipsu Lorem Ipsu Lorem IpsuLorem Ipsu",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Row(
+                children: [
+                  FilledButton(
+                    // onpressed function
+                    onPressed: () {
+                      print("buy now");
+                    },
+                    child: Text("Buy Now"),
+                  ),
+                  FilledButton(
+                    // onpressed function
+                    onPressed: () {
+                      print("Add a Bag");
+                    },
+                    child: Text("Add a Bag"),
+                  ),
+                ],
+              )
             ],
           ),
         ),
